bdd20a47b55335956e3d470f7a3e9d35c4c3ff8b
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-02-18 13:47:47 +0200

    pushed docker repo

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 import requests
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-BASE_URL = os.getenv("BASE_URL")
-CITY = os.getenv("CITY")
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Kyiv"
 
 
 def get_weather() -> None:
-    res = requests.get(BASE_URL + f"?q={CITY}&key={API_KEY}")
-    print("Performing request to Weather API for city Paris...")
+    res = requests.get(f"{BASE_URL}?q={CITY}&key={API_KEY}")
+    print("Performing request to Weather API for city Kyiv...")
     data = res.json()
     print(
         f"{data['location']['name']}/{data['location']['country']}"
